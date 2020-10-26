fde038e7c428c6c3b2868da03e924cf95c5fb80f
y7133 <41673178+y7133@users.noreply.github.com>
2020-10-27 02:31:27 +0900

    Update healthCheck.sh

--- scripts/healthCheck.sh
+++ scripts/healthCheck.sh
@@ -1,1 +1,28 @@
-curl "http://localhost:8080"
+#!/bin/bash
+echo "> Health check start"
+echo "> curl -s http://localhost:8080/actuator/health "
+
+for RETRY_COUNT in {1..15}
+do
+  RESPONSE=$(curl -s http://localhost:8080/actuator/health)
+  UP_COUNT=$(echo $RESPONSE | grep 'UP' | wc -l)
+
+  if [ $UP_COUNT -ge 1 ]
+  then # $up_count >= 1 ("UP" contains)
+      echo "> Health check success"
+      break
+  else
+      echo "> We don't know Health check reply or status is not UP"
+      echo "> Health check: ${RESPONSE}"
+  fi
+
+  if [ $RETRY_COUNT -eq 10 ]
+  then
+    echo "> Health check failed "
+    exit 1
+  fi
+
+  echo "> Health check connect failed again.."
+  sleep 10
+done
+exit 0
