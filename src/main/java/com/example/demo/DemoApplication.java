4d1a052cc127e7b70d9ec5e6419ec7a01e494676
y7133@naver.com <y7133@naver.com>
2020-10-26 21:25:24 +0900

    first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("jenkins CI/CD");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
